4b793c00b11d73217f721afd5aed5add4f0ea7b4
Arunkumar Paulraj <arunkumar.paulraj@hpe.com>
2026-06-30 09:48:04 +0530

    Updated the doc

--- doc.go
+++ doc.go
@@ -1,3 +1,3 @@
 // Package numtowords provides a function to convert numbers into their corresponding English words representation.
-
+// Supported range is from 0 to 9999. If the input number is outside this range, the function returns "Number out of range".
 package numtowords
